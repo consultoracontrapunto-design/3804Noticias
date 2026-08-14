59f59c08776ced34a123f209125289867ecc6659
consultoracontrapunto-design <consultoracontrapunto@gmail.com>
2026-08-14 19:08:14 -0300

    Create supabase-init.js

---
+++ js/supabase-init.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://qhuwyoiilizuctaumwea.supabase.co";
+const SUPABASE_KEY = "sb_publishable_7G8Tb3dsBycHCTJjhfRbsQ_vn0eIDkY";
+const sbClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
